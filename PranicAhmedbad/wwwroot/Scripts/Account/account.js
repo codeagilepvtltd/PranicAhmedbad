9aac11207e5ae0d59ada9d3de92537437120921d
dell <dell@DESKTOP-MKJE23I>
2024-02-17 13:31:05 +0530

    Add DevExtreme and create Role master

--- PranicAhmedbad/wwwroot/Scripts/Account/account.js
+++ PranicAhmedbad/wwwroot/Scripts/Account/account.js
@@ -1,5 +1,14 @@
 ﻿$(document).ready(function () {
+   
+});
 
-   
-   
+
+function ExportExcel() {
+    $("#grdSupplierDetials").dxDataGrid("instance").exportToExcel(false);
+}
+
+$.ajaxSetup({
+    data: {
+        __RequestVerificationToken: document.getElementsByName("__RequestVerificationToken")[0].value
+    }
 });
